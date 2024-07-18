336244b6fac955431dbfc7398bcb0e222b79b3a3
Youssef Ghattas <youssef.ghattas@integrant.com>
2024-07-18 19:17:11 +0300

    Initial Commit

---
+++ BackgroundDownload/BackgroundDownloadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundDownloadApp.swift
+//  BackgroundDownload
+//
+//  Created by Youssef Ghattas on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundDownloadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
